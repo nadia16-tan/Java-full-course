df85f27aa633e06fe76ad2a89e6d80acd4d475a9
Benit Polvie <polviedivine@gmail.com>
2026-05-09 16:41:52 +0200

    Create Exercise2_AgeVerification.java

---
+++ src/main/java/module02/Exercise2_AgeVerification.java
@@ -0,0 +1,11 @@
+// module02/Exercise2_AgeVerification.java
+package module02;
+
+public class Exercise2_AgeVerification {
+    public static void main(String[] args) {
+        int age = 18;
+
+        // Write your code below
+
+    }
+}
